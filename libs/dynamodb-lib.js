cd9ee72016e6aab32957e6c2bce9a99be9bcfc00
patiboh <lapatiboh@gmail.com>
2019-12-02 13:30:33 +0100

    🔧 Enable link to dev resources for dev* branches

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,9 +1,13 @@
 import AWS from 'aws-sdk';
+import config from '../config';
 
 AWS.config.update({region: `${process.env.region}`});
 
 export function call(action, params) {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  return dynamoDb[action](params).promise();
+  return dynamoDb[action]({
+    ...params,
+    TableName: `${config.resourcesStage}-${params.TableName}`
+  }).promise();
 }
