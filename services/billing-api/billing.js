fb4db56261ccb5e8c3a48b71848c65eed28c1f86
patiboh <lapatiboh@gmail.com>
2019-12-02 16:16:13 +0100

    🔧 [billing-api] Load stripe key per environment from AWS SSM

--- services/billing-api/billing.js
+++ services/billing-api/billing.js
@@ -1,13 +1,27 @@
+import AWS from '../../libs/aws-sdk';
 import stripePackage from 'stripe';
+
+import config from '../../config';
 import {calculateCost} from './libs/billing-lib';
 import {success, failure} from '../../libs/response-lib';
+
+const ssm = new AWS.SSM();
+
+/* Decrypt parameter outside Lambda handler */
+const stripeSecretKeyPromise = ssm
+  .getParameter({
+    Name: config.stripeKeyName,
+    WithDecryption: true
+  })
+  .promise();
 
 export async function main(event, context) {
   const {storage, source} = JSON.parse(event.body);
   const amount = calculateCost(storage);
   const description = 'Scratch charge';
 
-  const stripe = stripePackage(process.env.stripeSecretKey);
+  const stripeSecretKey = await stripeSecretKeyPromise;
+  const stripe = stripePackage(stripeSecretKey.Parameter.Value);
 
   try {
     await stripe.charges.create({
