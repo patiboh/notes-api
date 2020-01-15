3c520aa69f18fc1411b15a43189bd88c4777a94f
patiboh <lapatiboh@gmail.com>
2020-01-15 14:08:32 +0100

    🔧 Load admin phone from AWS secrets

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ const resourcesStage = process.env.resourcesStage;
 
 const stageConfigs = {
   dev: {
-    stripeKeyName: '/stripeSecretKey/test'
+    stripeKeyName: '/stripeSecretKey/test',
+    adminPhone: '/adminPhone/test'
   },
   prod: {
-    stripeKeyName: '/stripeSecretKey/live'
+    stripeKeyName: '/stripeSecretKey/live',
+    adminPhone: '/adminPhone/live'
   }
 };
 
